326b3add6baaf184e90307bcac2f8f39cf00ab68
alfonso <bsh0ugba@duck.com>
2024-02-03 03:37:39 +0100

    Improved the script to gather data for t-test.

--- run_100.sh
+++ run_100.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Define the opponent strategies as a variable
+opponent_strategies="grudger_recovery tit_for_tat_trustful win_stay_lose_shift always_cooperate always_defect tit_for_tat_suspicious alternate_coop random_70_cooperation"
+
 # Get the current date and time for the filename
 current_date_time=$(date +"%Y%m%d_%H%M")
 
@@ -9,11 +12,16 @@ filename="${current_date_time}.out"
 # Initialize an empty result string
 result=""
 
+# Save the parameters to the output file first
+echo "Parameters: $opponent_strategies" > "$filename"
+echo "Results:" >> "$filename"
+
+echo ""
 # Loop to run the command 100 times
 for i in {1..100}
 do
     echo "Running iteration $i of 100..."
-    output=$(/opt/homebrew/bin/python main.py -a grudger_recovery tit_for_tat_trustful win_stay_lose_shift always_cooperate always_defect tit_for_tat_suspicious alternate_coop random_70_cooperation | \
+    output=$(/opt/homebrew/bin/python main.py -a $opponent_strategies | \
     grep rl_strategy | grep Points | \
     awk -F', ' '{for(i=1; i<=NF; i++) if ($i ~ /Avg Points\/Game = /) { match($i, /Avg Points\/Game = [0-9.]+/); print substr($i, RSTART + 18, RLENGTH - 18); }}' )
 
@@ -25,11 +33,15 @@ done
 result="${result:2}"
 
 # Print the accumulated result (excluding the leading comma) on the screen
+echo ""
 echo "Accumulated Result:"
 echo "$result"
 
 # Save the accumulated result to a file
-echo "$result" > "$filename"
+echo "$result" >> "$filename"
 
 # Print a note on the screen
-echo "The accumulated result has been saved to $filename ."
+echo ""
+echo "The parameters and the accumulated result have been saved to $filename ."
+echo ""
+
